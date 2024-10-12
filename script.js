c8547817cdb13947d426fc7fe8bacc52cf6fc872
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-12 15:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const myElement = document.getElementById("fname");
+
+myElement.addEventListener("blur", function () {
+	myElement.value = myElement.value.toUpperCase();
+});
